3aec3792090ec37c0b78f206dc253e62c4f17f68
Pierre Gressier <pierre.gressier@leroymerlin.fr>
2020-12-23 18:21:06 +0100

    2.10 : Fiesta de los muertos : début implémentation

--- app/src/main/java/com/pigredorou/jeuxenvisio/FiestaDeLosMuertosActivity.java
+++ app/src/main/java/com/pigredorou/jeuxenvisio/FiestaDeLosMuertosActivity.java
@@ -1,15 +1,22 @@
 package com.pigredorou.jeuxenvisio;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 public class FiestaDeLosMuertosActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText mZoneSaisie;
+    Button mBoutonValider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,14 @@ public class FiestaDeLosMuertosActivity extends AppCompatActivity implements Vie
         ImageView boutonRetour = findViewById(R.id.bouton_retour);
         boutonRetour.setOnClickListener(this);
         boutonRetour.setImageResource(R.drawable.bouton_quitter);
+
+        // Saisie
+        mZoneSaisie = findViewById(R.id.zone_saisie);
+        mZoneSaisie.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        // Bouton valider
+        mBoutonValider = findViewById(R.id.bouton_valider);
+        mBoutonValider.setOnClickListener(this);
     }
 
     @Override
@@ -32,6 +47,10 @@ public class FiestaDeLosMuertosActivity extends AppCompatActivity implements Vie
         switch (v.getId()) {
             case R.id.bouton_retour:
                 finish();
+                break;
+            case R.id.bouton_valider:
+                Toast.makeText(this, mZoneSaisie.getText().toString(), Toast.LENGTH_SHORT).show();
+                ;
                 break;
         }
     }
