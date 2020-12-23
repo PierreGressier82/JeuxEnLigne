d1bf55cd34533005f9d30b407b7d6f793e515c16
Pierre Gressier <pierre.gressier@leroymerlin.fr>
2020-12-23 23:13:17 +0100

    2.10 : Fiesta de los muertos : début implémentation

--- app/src/main/java/com/pigredorou/jeuxenvisio/FiestaDeLosMuertosActivity.java
+++ app/src/main/java/com/pigredorou/jeuxenvisio/FiestaDeLosMuertosActivity.java
@@ -7,6 +7,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,8 @@ import java.util.Objects;
 
 public class FiestaDeLosMuertosActivity extends AppCompatActivity implements View.OnClickListener {
 
+    ImageView mImageCrane;
+    TextView mNomPersonnage;
     EditText mZoneSaisie;
     Button mBoutonValider;
 
@@ -28,16 +31,16 @@ public class FiestaDeLosMuertosActivity extends AppCompatActivity implements Vie
         // Affiche la vue
         setContentView(R.layout.activity_fiesta_de_los_muertos);
 
-        // Bouton retour
+        // Entête
         ImageView boutonRetour = findViewById(R.id.bouton_retour);
         boutonRetour.setOnClickListener(this);
         boutonRetour.setImageResource(R.drawable.bouton_quitter);
 
-        // Saisie
+        // Crane
+        mImageCrane = findViewById(R.id.image_crane);
+        mNomPersonnage = findViewById(R.id.nom_personnage);
         mZoneSaisie = findViewById(R.id.zone_saisie);
         mZoneSaisie.setInputType(InputType.TYPE_CLASS_TEXT);
-
-        // Bouton valider
         mBoutonValider = findViewById(R.id.bouton_valider);
         mBoutonValider.setOnClickListener(this);
     }
@@ -50,7 +53,8 @@ public class FiestaDeLosMuertosActivity extends AppCompatActivity implements Vie
                 break;
             case R.id.bouton_valider:
                 Toast.makeText(this, mZoneSaisie.getText().toString(), Toast.LENGTH_SHORT).show();
-                ;
+                mNomPersonnage.setVisibility(View.INVISIBLE);
+                mImageCrane.setImageResource(R.drawable.fiesta_crane_ferme_1);
                 break;
         }
     }
