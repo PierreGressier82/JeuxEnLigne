522b29fed826861c5729287d940b4031f86ee3c7
Pierre Gressier <pierre.gressier@leroymerlin.fr>
2021-01-20 23:36:12 +0100

    2.16 : - Le roi des nains : test drap & drop

--- app/src/main/java/com/pigredorou/jeuxenvisio/LeRoiDesNainsActivity.java
+++ app/src/main/java/com/pigredorou/jeuxenvisio/LeRoiDesNainsActivity.java
@@ -1,9 +1,15 @@
 package com.pigredorou.jeuxenvisio;
 
+import android.content.ClipData;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,6 +31,15 @@ public class LeRoiDesNainsActivity extends AppCompatActivity implements View.OnC
         ImageView boutonRetour = findViewById(R.id.bouton_retour);
         boutonRetour.setOnClickListener(this);
         boutonRetour.setImageResource(R.drawable.bouton_quitter);
+
+        // TEST DRAG & DROP
+        findViewById(R.id.table_carte_image_joueur1).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.table_carte_image_joueur2).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.table_carte_image_joueur3).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.table_carte_image_joueur4).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.table_carte_image_joueur5).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.source).setOnDragListener(new MyDragListener());
+        findViewById(R.id.cible).setOnDragListener(new MyDragListener());
     }
 
     @Override
@@ -33,6 +48,51 @@ public class LeRoiDesNainsActivity extends AppCompatActivity implements View.OnC
             case R.id.bouton_retour:
                 finish();
                 break;
+        }
+    }
+
+    private final class MyTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    class MyDragListener implements View.OnDragListener {
+        Drawable enterShape = getResources().getDrawable(R.drawable.shape_droptarget);
+        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    v.setBackground(enterShape);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                case DragEvent.ACTION_DRAG_ENDED:
+                    v.setBackground(normalShape);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign View to ViewGroup
+                    View view = (View) event.getLocalState();
+                    ViewGroup owner = (ViewGroup) view.getParent();
+                    owner.removeView(view);
+                    LinearLayout container = (LinearLayout) v;
+                    container.addView(view);
+                    view.setVisibility(View.VISIBLE);
+                    break;
+            }
+            return true;
         }
     }
 }
