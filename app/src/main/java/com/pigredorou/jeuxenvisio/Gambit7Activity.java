afbbe38c9a40d3cf0fc2ff0f29683655fc25162f
Pierre Gressier <pierre.gressier@leroymerlin.fr>
2021-01-31 23:51:13 +0100

    3.00 ALPHA 7 Ajout sablier de 30 sec

--- app/src/main/java/com/pigredorou/jeuxenvisio/Gambit7Activity.java
+++ app/src/main/java/com/pigredorou/jeuxenvisio/Gambit7Activity.java
@@ -2,10 +2,13 @@ package com.pigredorou.jeuxenvisio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.DragEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +22,11 @@ public class Gambit7Activity extends AppCompatActivity implements View.OnClickLi
     private String mPseudo; // Pseudo du joueur
     private int mIdSalon;
     private int mIdPartie;
-    private boolean mCommunicationFaite = false;
-    private boolean mCommunicationAChoisir = false;
-    private boolean mDetresseUtilise = false;
-    private int mNbTacheAAtribuer = 0;
-    private int mZoneSilence = 0;
-    private int mNumeroPli = 0;
-    private boolean mMajTerminee = true;
+    private TextView mTextView;
+    private ProgressBar mSablier;
+    private CountDownTimer mCompteurARebours;
+    private int mValeurProgression = 30;
+    private Button mBoutonValider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +36,7 @@ public class Gambit7Activity extends AppCompatActivity implements View.OnClickLi
         // Bloque la mise en veille
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         // Affiche la vue
-        setContentView(R.layout.activity_gambit_7);
+        setContentView(R.layout.activity_gambit7);
 
         // ENTETE
         TextView titre = findViewById(R.id.titre_jeu);
@@ -53,7 +54,13 @@ public class Gambit7Activity extends AppCompatActivity implements View.OnClickLi
         ImageView boutonRetour = findViewById(R.id.bouton_retour);
         boutonRetour.setOnClickListener(this);
         boutonRetour.setImageResource(R.drawable.bouton_quitter);
-
+        // Text
+        mTextView = findViewById(R.id.temps_restant);
+        mSablier = findViewById(R.id.sablier);
+        mBoutonValider = findViewById(R.id.bouton_valider);
+        mBoutonValider.setOnClickListener(this);
+        mValeurProgression = 30;
+        startChrono();
     }
 
     @Override
@@ -67,5 +74,26 @@ public class Gambit7Activity extends AppCompatActivity implements View.OnClickLi
     @Override
     public boolean onDrag(View v, DragEvent event) {
         return false;
+    }
+
+    private void startChrono() {
+        mSablier.setIndeterminate(false);
+        mSablier.setMax(mValeurProgression);
+        mSablier.setProgress(mValeurProgression);
+
+        mCompteurARebours = new CountDownTimer(30000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                String texte = String.valueOf(millisUntilFinished / 1000);
+                mTextView.setText(texte);
+                mSablier.setProgress((int) (millisUntilFinished / 1000));
+            }
+
+            public void onFinish() {
+                mBoutonValider.setOnClickListener(null);
+                mBoutonValider.setBackgroundColor(getResources().getColor(R.color.jaune));
+                mTextView.setText("Temps écoulé");
+            }
+        }.start();
+
     }
 }
